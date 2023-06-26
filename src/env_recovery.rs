cf1ccd6c817c9df1c88324c2d96667232593f06b
Michael Kielstra <pmkielstra@users.noreply.github.com>
2023-06-25 22:59:25 -0700

    Giving up on env-recovery

--- src/env_recovery.rs
+++
@@ -1,8 +0,0 @@
-let ENV_RECOVERY_VAR = "OR_RECOVERY"
-
-fn get_args() -> IntoIterator<String> {
-    match env::var(ENV_RECOVERY_VAR) {
-        Err(_) => std::env::args()
-        Ok(previous_command) => shellwords::split(previous_command)?
-    }
-}
